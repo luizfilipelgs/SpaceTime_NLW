919464cf36ffb0284a8d4bc921ed371703ad75eb
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-26 17:52:29 -0300

    chore: add depend back, prisma e sqlite

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,18 @@
 import { log } from 'console'
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const app = fastify()
 
-app.listen({
-  port: 3333,
-}).then(() => log('Server running on http://localhost:3333'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => log('Server running on http://localhost:3333'))
 
-app.get('/hello', () => 'hello word')
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
