00a09a2eb63cdae4a0d8b73346479116a5b63baa
luizfilipelgs <luizfilipelgs@gmail.com>
2023-06-09 19:40:23 -0300

    feat: home page

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -1,7 +1,77 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-logo.svg'
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        <div className="absolute bottom-2 right-2 top-0 w-2 bg-stripes" />
+
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="h10 flex w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-10 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-4xl font-bold leading-tight text-gray-50">
+              Sua Cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+            href=""
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        <div>
+          <p className="text-sm leading-relaxed text-gray-200">
+            Projeto realizado no evento NLW Ignite da Rocketseat.
+          </p>
+          <p className="text-sm leading-relaxed text-gray-200">
+            Acesse o repositorio em meu Github{' '}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="underline hover:text-gray-100"
+              href="https://github.com/luizfilipelgs/SpaceTime_NLW"
+            >
+              Aqui !
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50 ">
+              criar agora !
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
