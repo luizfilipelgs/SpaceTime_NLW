332c25372f4e47b56dfbf88b1f7f9737bedc4e22
luizfilipelgs <luizfilipelgs@gmail.com>
2023-06-10 01:02:57 -0300

    feat: add home pag

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,11 +1,80 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import {
+  ImageBackground,
+  View,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/logo.svg'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
+const StyledLogo = styled(Logo)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-4xl font-bold text-zinc-50">Hello World!</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900  px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <StyledLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text
+          className="text-center text-sm font-bold leading-relaxed text-gray-200"
+          onPress={() => {
+            Linking.openURL('https://github.com/luizfilipelgs/SpaceTime_NLW')
+          }}
+        >
+          Projeto realizado no evento NLW Ignite da Rocketseat, Acesse o
+          repositorio em meu Github
+        </Text>
+      </View>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
